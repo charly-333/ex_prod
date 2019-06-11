d3b8f6443043984e031d517c4352ccef483312b9
charly <charly333@free.fr>
2019-06-11 12:02:52 +0200

    publish ex1

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ console.log('initialisation server js');
 
 // acces a tout l'univers de express - qui est dans node_modules
 const express = require('express');
+const serveIndex = require('serve-index');
+
 const app = express();
-//console.log('express:',express);
 
 /*
 app.get('/', function (req, res) {
@@ -12,12 +13,17 @@ app.get('/', function (req, res) {
 */
 
 app.use(function (req, res, next) {
-    console.log('url',req.url);
+    console.log('url', req.url);
     next();
-  });
-  
-app.use(express.static('.'));
+});
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-})
+const dir = "../ex1/dist/ex1";
+
+app.use(express.static(dir));               //permet d'afficher le contenu des fichiers
+app.use(serveIndex(dir, {icons: true}));    // permet de naviguer dans le répertoire
+
+const port = 3000;
+app.listen(port, function () {
+    console.log('Server prod started on port: ' + port);
+});
+
