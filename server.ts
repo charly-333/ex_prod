04259efe06b0c37ae2ff9ae7f41d0f94ceb0570b
charly <charly333@free.fr>
2019-06-14 10:53:54 +0200

    get the quizz http method

--- server.ts
+++ server.ts
@@ -6,10 +6,12 @@ import * as express from 'express';     // notation de type ou es6
 //const serveIndex = require('serve-index');    //notation de node
 import * as serveIndex from 'serve-index';       // notation de type ou es6
 import * as cors from 'cors';
+import * as fs from 'fs';
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 /*
 app.get('/', function (req, res) {
@@ -19,23 +21,40 @@ app.get('/', function (req, res) {
 
 app.use(
   function (req, res, next) {
+    console.log('this', this);
     console.log('url', req.url);
     next();
-  }
-);
-
+  });
 // = aux "arrow fonction" :
 /* app.use( (req, res, next) => {
   console.log('url', req.url);
   next();
 }); */
-const dir: string = "../ex1/dist/ex1";
+
+let quizz;
+// methode post & url egale a /wz/quizz
+app.post('/ws/quizz', (req, res, next) => {
+  console.log('sync the quizz list', req.body);
+  quizz = req.body;
+  fs.writeFileSync('./quizz.json', JSON.stringify(quizz));
+  res.json({
+    message: 'sync ok'
+  });
+});
+
+app.get('/ws/quizz', (req, res, next) => {
+  console.log('get the quizz list');
+  quizz = JSON.parse(fs.readFileSync('./quizz.json', 'utf8'));
+  res.json(quizz);
+});
+
+const dir: string = '.';
 
 app.use(express.static(dir));               //permet d'afficher le contenu des fichiers
-app.use(serveIndex(dir, {icons: true}));    // permet de naviguer dans le répertoire
+app.use(serveIndex(dir, { icons: true }));    // permet de naviguer dans le répertoire
 
 const port = 3000;
 app.listen(port, function () {
-    console.log('Server prod started on port: ' + port);
+  console.log('Server prod started on port: ' + port);
 });
 
