2457a94b65f362c84c9aa6c66176be0579efe64b
charly <charly333@free.fr>
2019-06-14 10:07:51 +0200

    start nodemon server.ts

--- server.ts
+++ server.ts
@@ -1,10 +1,15 @@
 console.log('initialisation server js');
 
 // acces a tout l'univers de express - qui est dans node_modules
-const express = require('express');
-const serveIndex = require('serve-index');
+//const express = require('express');   // notation de node
+import * as express from 'express';     // notation de type ou es6
+//const serveIndex = require('serve-index');    //notation de node
+import * as serveIndex from 'serve-index';       // notation de type ou es6
+import * as cors from 'cors';
 
 const app = express();
+
+app.use(cors())
 
 /*
 app.get('/', function (req, res) {
@@ -12,12 +17,19 @@ app.get('/', function (req, res) {
 });
 */
 
-app.use(function (req, res, next) {
+app.use(
+  function (req, res, next) {
     console.log('url', req.url);
     next();
-});
+  }
+);
 
-const dir = "../ex1/dist/ex1";
+// = aux "arrow fonction" :
+/* app.use( (req, res, next) => {
+  console.log('url', req.url);
+  next();
+}); */
+const dir: string = "../ex1/dist/ex1";
 
 app.use(express.static(dir));               //permet d'afficher le contenu des fichiers
 app.use(serveIndex(dir, {icons: true}));    // permet de naviguer dans le répertoire
